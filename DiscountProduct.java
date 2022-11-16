9217a8ba7b0b0c29ed1b1678fbe4750f959e2641
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2022-11-16 22:45:36 +0100

    Delete DiscountProduct.java

--- DiscountProduct.java
+++
@@ -1,7 +0,0 @@
-class DiscountProduct extends Product{
-
-    public DiscountProduct(String name, double price, int amountLeft, Integer discount ) {
-        super(name, price, amountLeft, discount);
-    }
-    
-}
